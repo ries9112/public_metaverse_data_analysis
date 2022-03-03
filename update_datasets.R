6b6a35f9639034b6dda7fd89953bf200b25b3e14
ries9112 <32306488+ries9112@users.noreply.github.com>
2022-03-03 21:09:21 +0100

    Create update_datasets.R

---
+++ update_datasets.R
@@ -0,0 +1,14 @@
+library(pins)
+
+# Register Board for data pull
+board_register("https://raw.githubusercontent.com/predictcrypto/pins/master/","pins_data")
+
+# Pull data
+cryptovoxels_all_parcels = pin_get("cryptovoxels_all_parcels", "pins_data")
+cryptovoxels_orders = pin_get("cryptovoxels_orders", "pins_data")
+decentraland_thegraph = pin_get("decentraland_thegraph", "pins_data")
+
+# Write csv
+write.csv(cryptovoxels_all_parcels, "cryptovoxels/data/cryptovoxels_all_parcels.csv")
+write.csv(cryptovoxels_orders, "cryptovoxels/data/cryptovoxels_orders.csv")
+write.csv(decentraland_thegraph, "cryptovoxels/data/decentraland_thegraph.csv")
